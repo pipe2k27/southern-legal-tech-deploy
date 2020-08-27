170b0a2f35a14ae7ac4fad37d4075012ff85e8bf
felipe <feliperarnau@gmail.com>
2020-08-27 15:23:54 -0300

    Updates

---
+++ precache-manifest.80a6b063513e84809a0c7c8a4c11ef8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f0b2f0aceee9a1b7d69808bb6d6f4d",
+    "url": "/southern-legal-tech-deploy/index.html"
+  },
+  {
+    "revision": "d1597097193e5024679a",
+    "url": "/southern-legal-tech-deploy/static/css/main.52854ea1.chunk.css"
+  },
+  {
+    "revision": "f2ab2402457ef6494006",
+    "url": "/southern-legal-tech-deploy/static/js/2.7e365608.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/southern-legal-tech-deploy/static/js/2.7e365608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1597097193e5024679a",
+    "url": "/southern-legal-tech-deploy/static/js/main.5f941341.chunk.js"
+  },
+  {
+    "revision": "395df2bf4e4c6950ced6",
+    "url": "/southern-legal-tech-deploy/static/js/runtime-main.2d152d61.js"
+  }
+]);
